f16f7d0bb442ec82518be574d870b8fcae3bee5e
archaicmuse <archaicmuse@gmail.com>
2015-10-16 18:01:06 +0200

    ardruino moteurs

---
+++ Arduino/SketchMoteurs.ino
@@ -0,0 +1,45 @@
+#include <AccelStepper.h>                   //importing the AccelStepper library
+
+const float angularStep = 0.9;
+const unsigned short int stepsPerRevolution = 360/angularStep;
+const unsigned short int resolutionX = 80;                       // must be an integer
+const unsigned short int resolutionY = 80;
+
+AccelStepper Xaxis(1, 3, 6);                 //steppers initialization (type, step pin, direction pin)
+AccelStepper Yaxis(1, 4, 7);
+
+void setup() {
+    
+  Xaxis.moveTo(0);                            //steppers reset in neutral position
+  Xaxis.runToPosition();
+  Yaxis.moveTo(0);                  
+  Yaxis.runToPosition(); 
+}
+
+void loop(){    
+  
+  Xaxis.moveTo((int)(-0.5)*resolutionX);      //initial X displacement instruction(extreme left, cut)
+  Xaxis.runToPosition();                      //instruction process
+  Yaxis.moveTo((int)0.5*resolutionY);         //initial Y displacement instruction (extreme high, cut)
+  Yaxis.runToPosition();                      //instruction process
+
+  for(int yi = 1; yi <= resolutionY; yi++) {
+    //read IR sensor
+
+    for(int xi = 1; xi <= resolutionX; xi++){
+      Xaxis.moveTo(Xaxis.currentPosition() - pow(-1, yi % 2)); //X line movement
+      Xaxis.runToPosition();
+
+      //read IR sensor
+    }
+    
+    Yaxis.moveTo(Yaxis.currentPosition() -1); //Y column movement
+    Yaxis.runToPosition();
+  }
+
+  Xaxis.moveTo(0);                          // steppers reset in neutral position
+  Xaxis.runToPosition();
+  Yaxis.moveTo(0);                  
+  Yaxis.runToPosition();        
+}
+
