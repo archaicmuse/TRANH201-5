b90798d31d85f056c1835a9f9dc642c404519b95
archaicmuse <archaicmuse@gmail.com>
2015-10-20 15:18:54 +0200

    Code allégé

---
+++ Arduino/ProtoCompletLITE/ProtoCompletLITE.ino
@@ -0,0 +1,38 @@
+#include <AccelStepper.h>
+
+byte resolutionX = 80;
+byte resolutionY = 80;
+const int analogPin = A0;
+AccelStepper Xaxis(1, 3, 6);
+AccelStepper Yaxis(1, 4, 7);
+
+void setup(){
+  Serial.begin(9600);
+  stepsDisplacement(0,0);
+}
+
+void stepsDisplacement(byte x, byte y){
+  Xaxis.moveTo(x);
+  Xaxis.runToPosition();
+  Yaxis.moveTo(y);                  
+  Yaxis.runToPosition();
+}
+
+
+void loop(){
+  stepsDisplacement((byte)(-0.5)*resolutionX, (byte)0.5*resolutionY);
+  Serial.write(resolutionX);
+  for(byte yi = 0; yi < resolutionY; yi++) {
+    Serial.write((int)analogRead(analogPin));  
+    short int direction = pow(-1, yi % 2);
+    for(byte xi = 0; xi < resolutionX; xi++){
+      Xaxis.move(direction);
+      Xaxis.runToPosition();
+      Serial.write((int)analogRead(analogPin));
+    }
+    Yaxis.move(-1);
+    Yaxis.runToPosition();
+  }
+  stepsDisplacement(0,0);
+  delay(3600000);
+}
