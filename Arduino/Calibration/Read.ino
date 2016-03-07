c7b6a7dd6c1619a9620f334908d4e9be74e5c1ca
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2016-03-07 22:01:07 +0100

    Added files via upload

---
+++ Arduino/Calibration/Read.ino
@@ -0,0 +1,49 @@
+#include <i2cmaster.h>
+
+int dev =  0x00;
+int version = 0;
+
+
+unsigned int I2CRead(int adress){
+  i2c_start_wait(dev+I2C_WRITE);
+  i2c_write(adress);
+  i2c_rep_start(dev+I2C_READ);
+  unsigned int LSB = i2c_readAck();
+  unsigned int MSB = i2c_readAck();
+  unsigned int pec = i2c_readNak();
+  i2c_stop();
+  unsigned int regValue = (((MSB) << 8) + LSB);
+  return regValue;
+  delay(100);
+}
+
+void setup()
+{
+//Make sure you set the Baudrate inside the Serial Window to 9600
+Serial.begin(9600);
+Serial.println("This program will read the EEPROM settings of your sensor");
+Serial.println("");
+Serial.println("PLEASE NOTE DOWN CAREFULLY THE FOLLOWING VALUES !");
+Serial.println("");
+
+//Initialising the I2C Bus and activating the internal pullup resistors
+i2c_init();
+PORTC = (1 << PORTC4) | (1 << PORTC5); 
+
+//Reading the previous values
+Serial.print("Filter settings: ");
+Serial.println(I2CRead(0x25));
+
+Serial.print("Maximum temp: ");
+Serial.println(I2CRead(0x20));
+
+Serial.print("Minimum temp: ");
+Serial.println(I2CRead(0x21));
+}
+
+
+void loop()
+{
+}
+
+
---
+++ Arduino/Calibration/Write.ino
@@ -0,0 +1,22 @@
+#include <i2cmaster.h>
+
+int dev =  0x00;
+int version = 0;
+
+void setup() {
+  i2c_init();
+  PORTC = (1 << PORTC4) | (1 << PORTC5); 
+  I2CWrite(0x25,0xB4,0xB7,0x94);
+}
+
+void loop() {}
+
+void I2CWrite(int adress, unsigned int LSB, unsigned int MSB, int PEC){
+  i2c_start_wait(dev+I2C_WRITE);
+  i2c_write(adress);
+  i2c_write(LSB); 
+  i2c_write(MSB);
+  i2c_write(PEC);
+  i2c_stop();  
+  delay(100);
+}
