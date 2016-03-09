7d5481ee8547484ca3e4e6b90656a83663233f96
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2016-03-09 13:23:32 +0100

    Code works!

--- Arduino/Sketch.ino
+++ Arduino/Sketch.ino
@@ -1,36 +1,46 @@
-#include <Servo.h>
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
+#include <Servo.h>
 
 byte resolutionX = 80;
 byte resolutionY = 80;
-
+byte xTop = 90 - resolutionX/2;
+byte yTop = 90 + resolutionY/2;
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
-Servo servoX;
-Servo servoY;
+Servo ServoX;
+Servo ServoY;
 
 void setup(){
   Serial.begin(9600);
-  mlx.begin(); 
-  servoX.attach(9);
-  servoY.attach(10);
+  mlx.begin();
+  ServoX.attach(10);
+  ServoY.attach(9);
 }
 
 void loop(){
-  if (Serial.available() > 0) {
-    for(byte yi = 0; yi < resolutionY; yi++) {
-      Serial.print(mlx.readObjectTempC());
-      short int direction = -1*pow(-1, yi%2);
-      for(byte xi = 0; xi < resolutionX; xi++){
-        servoX.write((byte)(-0.5)*resolutionX*direction + direction*xi + 0.5*(direction - 1) + 90);
-        delay(20);
-        Serial.print(mlx.readObjectTempC());
+  if(Serial.available() > 0){
+    ServoX.write(xTop);
+    ServoY.write(yTop);
+    for(byte yi = yTop; yi > yTop - resolutionY; yi--){
+      short int direction = -1*pow(-1, yi % 2);
+      ServoY.write(yi);
+      if(direction == 1){
+        for(byte xi = xTop; xi < xTop + resolutionX; xi++){
+          ServoX.write(xi);
+          delay(20);
+          Serial.println(mlx.readObjectTempC());
+          
+        }
+      } else {
+        for(byte xi = xTop + resolutionX; xi > xTop; xi--){
+          ServoY.write(xi);
+          delay(20);
+          Serial.println(mlx.readObjectTempC());
+        }
       }
-      servoY.write((byte)0.5*resolutionY - yi + 90);
-      delay(20);
     }
-    servoX.write(90);
-    servoY.write(90);
+    ServoX.write(90);
+    ServoY.write(90);
     exit(0);
   }
 }
