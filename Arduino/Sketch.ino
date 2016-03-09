7f88e3e8292bc8ad73c35e5e48d51d1b8bcd4d50
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2016-03-09 15:48:54 +0100

    Update Sketch.ino

--- Arduino/Sketch.ino
+++ Arduino/Sketch.ino
@@ -11,36 +11,35 @@ Servo ServoX;
 Servo ServoY;
 
 void setup(){
-  Serial.begin(9600);
-  mlx.begin();
-  ServoX.attach(10);
-  ServoY.attach(9);
+    Serial.begin(9600);
+    mlx.begin();
+    ServoX.attach(10);
+    ServoY.attach(9);
 }
 
 void loop(){
-  if(Serial.available() > 0){
-    ServoX.write(xTop);
-    ServoY.write(yTop);
-    for(byte yi = yTop; yi > yTop - resolutionY; yi--){
-      short int direction = -1*pow(-1, yi % 2);
-      ServoY.write(yi);
-      if(direction == 1){
-        for(byte xi = xTop; xi < xTop + resolutionX; xi++){
-          ServoX.write(xi);
-          delay(20);
-          Serial.println(mlx.readObjectTempC());
-          
+    if(Serial.available() > 0) {
+        ServoX.write(xTop);
+        ServoY.write(yTop);
+        for(byte yi = yTop; yi > yTop - resolutionY; yi--) {
+            short int direction = -1*pow(-1, yi % 2);
+            ServoY.write(ServoY.read() - 1);
+            if(direction == 1) {
+                for(byte xi = xTop; xi < xTop + resolutionX; xi++) {
+                    ServoX.write(ServoX.read() + 1);
+                    delay(20);
+                    Serial.println(mlx.readObjectTempC());
+                }
+            } else {
+                for(byte xi = xTop + resolutionX; xi > xTop; xi--) {
+                    ServoX.write(ServoX.read() - 1);
+                    delay(20);
+                    Serial.println(mlx.readObjectTempC());
+                }
+            }
         }
-      } else {
-        for(byte xi = xTop + resolutionX; xi > xTop; xi--){
-          ServoY.write(xi);
-          delay(20);
-          Serial.println(mlx.readObjectTempC());
-        }
-      }
+        ServoX.write(90);
+        ServoY.write(90);
+        exit(0);
     }
-    ServoX.write(90);
-    ServoY.write(90);
-    exit(0);
-  }
 }
