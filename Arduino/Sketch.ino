7ab7d50b6d900dba33a0a903c4aecc1b0968488a
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2016-03-09 22:57:43 +0100

    Add button & SD card

--- Arduino/Sketch.ino
+++ Arduino/Sketch.ino
@@ -1,42 +1,71 @@
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
 #include <Servo.h>
+#include <SPI.h>
+#include <SD.h>
 
 byte resolutionX = 80;
 byte resolutionY = 80;
 byte xTop = 90 - resolutionX/2;
 byte yTop = 90 + resolutionY/2;
+String Line;
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 Servo ServoX;
 Servo ServoY;
+File data;
 
 void setup(){
     Serial.begin(9600);
     mlx.begin();
     ServoX.attach(10);
     ServoY.attach(9);
+    pinMode(2, INPUT);
 }
 
 void loop(){
-    if(Serial.available() > 0) {
+    int buttonState = digitalRead(2);
+    if(Serial.available() > 0 || buttonState == HIGH) {
         ServoX.write(xTop);
         ServoY.write(yTop);
+        if(SD.begin(4)){
+            data = SD.open("data.csv", FILE_WRITE);
+        }
         for(byte yi = yTop; yi > yTop - resolutionY; yi--) {
             short int direction = -1*pow(-1, yi % 2);
-            ServoY.write(ServoY.read() - 1);
+
             if(direction == 1) {
                 for(byte xi = xTop; xi < xTop + resolutionX; xi++) {
                     ServoX.write(ServoX.read() + 1);
                     delay(20);
-                    Serial.println(mlx.readObjectTempC());
+                    double temperature = mlx.readObjectTempC();
+                    Serial.println(temperature);
+                    Line.concat(String(temperature));
+                    if (xi != xTop + resolutionX - 1) {
+                        Line.concat(",");
+                    }
                 }
             } else {
                 for(byte xi = xTop + resolutionX; xi > xTop; xi--) {
                     ServoX.write(ServoX.read() - 1);
                     delay(20);
-                    Serial.println(mlx.readObjectTempC());
+                    double temperature = mlx.readObjectTempC();
+                    Serial.println(temperature);
+                    Line.concat(String(temperature));
+                    if (xi != xTop + 1) {
+                        Line.concat(",");
+                    }
                 }
             }
+            if(SD.begin(4)){
+                if(data){
+                    data.println(Line);
+                    Line = "";
+                }
+            }
+            ServoY.write(ServoY.read() - 1);
+        }
+        if(SD.begin(4)){
+            data.close();
         }
         ServoX.write(90);
         ServoY.write(90);
